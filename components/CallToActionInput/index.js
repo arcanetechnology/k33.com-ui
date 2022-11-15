f69becae82560134f717a352b3963238c5d14c83
Radoslav Popov <shadow_87@mail.bg>
2022-11-15 10:54:25 +0000

    Fix lint error

--- components/CallToActionInput/index.js
+++ components/CallToActionInput/index.js
@@ -9,7 +9,7 @@ export default function CallToActionInput({ className }) {
   return (
     <div className={cn(styles.inputWrapper, { [className]: !!className })}>
       <input type="email" placeholder="Enter email address" disabled />
-      <Button onClick={() => { setIsPopupOpen(true) }}>
+      <Button onClick={() => { setIsPopupOpen(true); }}>
         Get Started
       </Button>
 

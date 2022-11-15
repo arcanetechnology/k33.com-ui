46cc19e7e56f3aea299821530337cc0ecc0834ee
Radoslav Popov <shadow_87@mail.bg>
2022-11-15 10:53:30 +0000

    Add popup

--- components/CallToActionInput/index.js
+++ components/CallToActionInput/index.js
@@ -1,14 +1,23 @@
-import { Button } from '@arcanetechnology/react-ui-lib/lib/index.cjs';
+import { Button, Popup } from '@arcanetechnology/react-ui-lib';
 import cn from 'classnames';
+import { useState } from 'react';
 import styles from './index.module.scss';
 
 export default function CallToActionInput({ className }) {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   return (
     <div className={cn(styles.inputWrapper, { [className]: !!className })}>
       <input type="email" placeholder="Enter email address" disabled />
-      <Button onClick={() => { alert('Work in progress'); }}>
+      <Button onClick={() => { setIsPopupOpen(true) }}>
         Get Started
       </Button>
+
+      <Popup showCloseIcon isOpen={isPopupOpen} onClose={() => { setIsPopupOpen(false); }}>
+        <div style={{ padding: '32px 8px', fontSize: '22px' }}>
+          Work in Progress!
+        </div>
+      </Popup>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import HomePage from 'components/HomePage';
 
 export default function Home() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <HomePage />
   );
 }

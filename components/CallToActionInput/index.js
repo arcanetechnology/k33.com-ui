fe3a420900646765207e51a64f16981f27d9fde8
Radoslav Popov <shadow_87@mail.bg>
2022-11-17 12:27:51 +0000

    Handling mail sending better

--- components/CallToActionInput/index.js
+++ components/CallToActionInput/index.js
@@ -5,23 +5,48 @@ import styles from './index.module.scss';
 
 export default function CallToActionInput({ className, label }) {
   const [email, setEmail] = useState('');
+  const [isSending, setIsSending] = useState(false);
 
   const sendEmail = async () => {
-    await fetch('/api/mail', {
+    setIsSending(true);
+
+    const result = await fetch('/api/mail', {
       method: 'POST',
       body: JSON.stringify({
         email
       })
     });
+
+    if (result.status !== 200) {
+      alert('error');
+    } else {
+      alert('success');
+      setEmail('');
+    }
+
+    setIsSending(false);
+  };
+
+  const handleKey = async (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      await sendEmail();
+    }
   };
 
   return (
     <div className={cn(styles.inputWrapper, { [className]: !!className })}>
       <div className={styles.emailWrapper}>
-        <input type="email" placeholder="Enter your email" onChange={(e) => { setEmail(e.target.value); }} />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => { setEmail(e.target.value); }}
+          onKeyDown={handleKey}
+        />
       </div>
 
-      <Button onClick={sendEmail}>
+      <Button onClick={sendEmail} disabled={isSending}>
         {label || 'Get Started'}
       </Button>
     </div>

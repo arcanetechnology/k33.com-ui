a37207df94d5f53f78213d57a43c1289e0954277
Radoslav Popov <shadow_87@mail.bg>
2022-11-23 15:02:30 +0000

    Fix grid menu for dark mode

--- svg/helpers/getColorScheme.js
+++ svg/helpers/getColorScheme.js
@@ -5,5 +5,9 @@ export const getColorScheme = ({ dark, active }) => {
 
   const fillOpacity = active ? 1 : 0.3;
 
-  return { fill, fillOpacity };
+  const textColor = dark
+    ? active ? '#090A0B' : 'rgba(235, 235, 245, 0.3)'
+    : 'white';
+
+  return { fill, fillOpacity, textColor };
 };

660380b0ea381959a6672f0c1c53f190157f6203
Radoslav Popov <shadow_87@mail.bg>
2023-01-31 11:43:31 +0000

    Add Vinter page

---
+++ pages/investments/products/kvq.js
@@ -0,0 +1,96 @@
+import { Button } from '@arcanetechnology/react-ui-lib';
+import HeroTopSection from 'components/HeroTopSection';
+import Page from 'components/Page';
+import Head from 'next/head';
+import Image from 'next/image';
+import ArrowLeft from 'svg/ArrowLeft';
+import ArrowRight from 'svg/ArrowRight';
+import ExternalLink from 'svg/ExternalLink';
+import __text__ from 'text/kvq';
+import styles from './index.module.scss';
+
+/**
+ * Represents the KVQ product page.
+ */
+export default function KvqPage() {
+  return (
+    <Page subLogo={{ image: '/investments-logo.png' }}>
+      <Head>
+        <title>KVQ | K33</title>
+      </Head>
+
+      <HeroTopSection
+        oneLiner
+        largerSubText
+        noCallToActionButton
+        heroText={__text__.HERO}
+        subText={__text__.HERO_SUBTEXT}
+        leftImage={<Image src="/kvq-left-bg.png" width={147} height={424} alt="" />}
+        rightImage={<Image src="/kvq-right-bg.png" width={145} height={410} alt="" />}
+      />
+
+      <section className={styles.contentSection}>
+        <h2 className={styles.title}>{__text__.TITLE}</h2>
+
+        <div className={styles.contentWrapper}>
+          <div className={styles.text}>
+            {__text__.DESCRIPTION.map((line) => (
+              <p key={line}>{line}</p>
+            ))}
+          </div>
+
+          <div className={styles.contacts}>
+            <div className={styles.contactTitle}>{__text__.CONTACT}</div>
+
+            <ul className={styles.contactList}>
+              <li>
+                <div className={styles.image}>
+                  <Image src="/Anders.png" alt="Anders Helseth" width={88} height={88} />
+                </div>
+                <div className={styles.content}>
+                  <div className={styles.name}>Anders Helseth</div>
+                  <div className={styles.email}>anders@k33.com</div>
+                </div>
+              </li>
+
+              <li>
+                <div className={styles.image}>
+                  <Image src="/Torbjorn.png" alt="Torbjørn Bull Jenssen" width={88} height={88} />
+                </div>
+                <div className={styles.content}>
+                  <div className={styles.name}>Torbjørn Bull Jenssen</div>
+                  <div className={styles.email}>tbj@k33.com</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={styles.buttons}>
+          <Button>Learn More About</Button>
+          <Button secondary iconRight={<ExternalLink />}>Go To Vinter</Button>
+        </div>
+      </section>
+
+      <section className={styles.selectionSection}>
+        <div className={styles.prevNext}>
+          <div className={styles.prevNextItem}>
+            <ArrowLeft />
+            <div className={styles.prevText}>Last selection and rebalancing date : February 1st 2023</div>
+          </div>
+
+          <div className={styles.prevNextItem}>
+            <div className={styles.nextText}>Next selection and rebalancing date : May 1st 2023</div>
+            <ArrowRight />
+          </div>
+        </div>
+
+        <div className={styles.title}>
+          {__text__.SELECTION_TITLE}
+        </div>
+
+        <img src="/k33-vinter-quality-index-selection.png" className={styles.mainImage} alt="K33 Vinter quality index selection" />
+      </section>
+    </Page>
+  );
+}
---
+++ text/kvq.js
@@ -0,0 +1,19 @@
+/* eslint-disable max-len */
+const __text__ = {
+
+  HERO: 'K33-Vinter Quality Index (KVQ)',
+  HERO_SUBTEXT: 'You could write something here',
+
+  TITLE: 'What KVQ is?',
+
+  DESCRIPTION: [
+    'The K33-Vinter index is a smart beta index for crypto assets. The index consists of an equal weighted mix of the most promising tokens amongst top 30 crypto assets. Tokens with a low long term survival-probability are excluded, contributing to a substantial reduction in downside risk in the index.',
+    'For inclusion in the index, a top 30 token must pass the Quality Filter. The purpose of the Quality Filter is to evaluate the quality of crypto assets from an investor’s standpoint. A high-quality asset is defined as an asset having a small probability of permanent financial loss while a low-quality asset possess a large probability of permanent financial loss.'
+  ],
+
+  CONTACT: 'Contact',
+
+  SELECTION_TITLE: 'Current selection (Number indicates ranking by market cap on January 25th, 2023)'
+};
+
+export default __text__;
